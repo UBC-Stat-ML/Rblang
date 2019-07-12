66f6c04176fcca7e3c6562dea5420ab5221844f6
Sahand-H <sahandhosseinii@gmail.com>
2019-07-12 14:04:45 -0700

    Update installSDK.R

--- R/installSDK.R
+++ R/installSDK.R
@@ -2,9 +2,6 @@
 #' Checks to see if blangSDK has already been installed, and whether or not the PATH to the Command Line Interface is set up correctly.
 setupSDK <- function() {
   olddir <- getwd()
-  # if (!file_test("-d", paste(find.package(package = "Rblang"),"/blang",sep = ""))) {
-  #   installSDK()
-  # }
 
   instPath <- paste(find.package(package = "Rblang"),"/blang",sep = "")
   blangPath <- paste(instPath,"/blangSDK-master/build/install/blang/bin/",sep = "")
@@ -28,19 +25,12 @@ installSDK <- function(olddir, instPath) {
 
   unzip(zipfile = paste(instPath,"blangSDK-master.zip",sep = "/"), exdir = instPath)
 
-  #system(paste("cd ", instPath, "/blangSDK-master/", sep = ""))
-  #Sys.chmod(paste(instPath, "/blangSDK-master/setup-cli.sh",sep = ""), "777")
-  #system(paste("source ", instPath, "/set-permission.sh", sep=""))
-  #system(paste("chmod +x * ", sdkPath, "/setup-cli.sh", sep=""))
-  #setwd(sdkPath)
+
   system(paste("cd ", sdkPath, "/ && ",
                "chmod +x * setup-cli.sh && ",
                paste("source ", instPath, "/blangSDK-master/setup-cli.sh &&", sep = ""),
                paste("cd ", olddir, sep=""),
                sep = ""))
-
-  #system(paste("source ", instPath, "/blangSDK-master/setup-cli.sh",sep = ""))
-  #setwd(olddir)
 }
 
 #' Checks for existence of blangSDK Command Line Interface files
